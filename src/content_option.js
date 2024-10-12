dfeb4ec415112a71b2a19c01ecb103c69b825087
Jihad Sayed <gehadsaed199@gmail.com>
2024-10-12 15:42:06 +0300

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,175 @@
+const logotext = "Jihad";
+const meta = {
+    title: "Jihad Sayed",
+    description: "I’m Jihad Sayed, a Frontend Developer and Designer website",
+};
+
+const introdata = {
+    title: "I’m Jihad Sayed",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I Frontend Developer",
+    },
+    description: "I develop innovative user interfaces that combine attractive design with advanced functionality. My goal is to build interactive websites that meet users' needs and deliver exceptional performance across various devices.",
+    your_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvNykSNhnJYAvWZsFhMp9gwu1wVJpeEiz2Pg&s",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am Jihad, a responsive front-end developer. I graduated from a college of commerce. I started in the field of front-end development two years ago for the love of the field and studied it through an online course in the city of Zagazig. I completed an online course from the YouTube platforms and enjoyed the skills and designed many websites with UI & UX in mind. I also study a UI course. & UX and constantly develop my skills.",
+};
+
+//         jobtitle: "Designer of week",
+//         where: "YAdfi",
+//         date: "2020",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "Jamalya",
+//         date: "2019",
+//     },
+//     {
+//         jobtitle: "Designer of week",
+//         where: "ALquds",
+//         date: "2019",
+//     },
+// ];
+
+const skills = [{
+        name: "Html",
+        value: 90,
+    },
+    {
+        name: "Css",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Jquery",
+        value: 60,
+    },
+    {
+        name: "Bootstrap",
+        value: 80,
+    },
+    {
+        name: "Tailwind",
+        value: 85,
+    },
+    {
+        name: "Git",
+        value: 60,
+    },
+
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Design that takes into account all the basics of UI & UX and according to the needs of the family and what is compatible with their work or the entity responsible for the site..",
+    },
+    {
+        title: "Web Development",
+        description: "A responsive and streamlined website in dealing with the user and the user and creating everything that is needed according to the client’s requirements and the work he wants, while taking into account an attractive and smooth experience..",
+    },
+  
+];
+
+const dataportfolio = [{
+        img: "/image1.png",
+        description: "A modern e-commerce store built with React, featuring a user-friendly interface, secure payments, and efficient product browsing..",
+        link: "https://perfacto.netlify.app/",
+    },
+    {
+        img: "/image2.png",
+        description: "A personal website made with Bootstrap, responsive to all screens.",
+        link: "https://gehadsayed1.github.io/the-first-exam/",
+    },
+    {
+        img: "/image3.png",
+        description: "A personal website made with Bootstrap, responsive to all screens.",
+        link: "https://templettow.netlify.app/",
+    },
+    {
+        img: "/image4.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://grand-kheer-42a489.netlify.app/",
+    },
+    {
+        img: "/image5.png",
+        description: "Responsive to-do list, you can add an image.",
+        link: "https://gehadsayed1.github.io/task-list/",
+    },
+    {
+        img: "/image6.png",
+        description: "The dash board is organized, has everything that will be needed, and is responsive.",
+        link: "https://gehadsayed1.github.io/Dashbord/",
+    },
+
+    {
+        img: "/image7.png",
+        description: " website made with css, responsive to all screens.",
+        link: "https://gehadsayed1.github.io/template-three/",
+    },
+    {
+        img: "/image8.png",
+        description: " website made with css, responsive to all screens.",
+        link: "https://gehadsayed1.github.io/p5web/",
+    },
+    {
+        img: "/image9.png",
+        description: " website made with css ,html,js, responsive to all screens.",
+        link: "https://gehadsayed1.github.io/temple-tow/",
+    },
+    {
+        img: "/image10.png",
+        description: " website made with css ,html,js, responsive to all screens.",
+        link: "https://gehadsayed1.github.io/jobs/",
+    },
+    {
+        img: "/image11.png",
+        description: " website made with css ,html,js,bootstrap, responsive to all screens..",
+        link: "https://gehadsayed1.github.io/template-bootstrap/",
+    },
+    {
+        img: "/image12.png",
+        description: " website made with css ,html,js,bootstrap, responsive to all screens..",
+        link: "https://gehadsayed1.github.io/template-three/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "gehadsaed199@gmail.com",
+    YOUR_FONE: "+201033671306",
+    description: "Contact me at any time. I will always be at your service to provide what you need, whatever it is, in the fastest time and with high quality. Thank you for contacting me.. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_lexgsjl",
+    YOUR_TEMPLATE_ID: "template_gq9f0hn",
+    YOUR_USER_ID: "QR_yTc93M0JDJwxC-",
+};
+
+const socialprofils = {
+    github: "https://github.com/gehadsayed1 ",
+    facebook: "https://www.facebook.com/profile.php?id=100026066917383&mibextid=ZbWKwL",
+    linkedin: "https://www.linkedin.com/in/jihad-sayed-4a51172bb",
+    WhatsApp: "https://wa.me/201033671306",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
