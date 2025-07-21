35d3ac1130860dc7d24ea2bc99f96c76cb194dbe
jihad <gehadsaed199@gmail.com>
2025-07-22 01:17:18 +0300

    add project

--- src/content_option.js
+++ src/content_option.js
@@ -37,112 +37,138 @@ const dataabout = {
 // ];
 
 const skills = [{
-        name: "Html",
-        value: 90,
-    },
-    {
-        name: "Css",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 70,
-    },
-    {
-        name: "Jquery",
-        value: 60,
-    },
-    {
-        name: "Bootstrap",
-        value: 80,
-    },
-    {
-        name: "Tailwind",
-        value: 85,
-    },
-    {
-        name: "Git",
-        value: 60,
-    },
+    name: "Html",
+    value: 90,
+},
+{
+    name: "Css",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 70,
+},
+{
+    name: "Jquery",
+    value: 60,
+},
+{
+    name: "Bootstrap",
+    value: 80,
+},
+{
+    name: "Tailwind",
+    value: 85,
+},
+{
+    name: "Git",
+    value: 60,
+},
 
 ];
 
 const services = [{
-        title: "UI & UX Design",
-        description: "Design that takes into account all the basics of UI & UX and according to the needs of the family and what is compatible with their work or the entity responsible for the site..",
-    },
-    {
-        title: "Web Development",
-        description: "A responsive and streamlined website in dealing with the user and the user and creating everything that is needed according to the client’s requirements and the work he wants, while taking into account an attractive and smooth experience..",
-    },
-  
+    title: "UI & UX Design",
+    description: "Design that takes into account all the basics of UI & UX and according to the needs of the family and what is compatible with their work or the entity responsible for the site..",
+},
+{
+    title: "Web Development",
+    description: "A responsive and streamlined website in dealing with the user and the user and creating everything that is needed according to the client’s requirements and the work he wants, while taking into account an attractive and smooth experience..",
+},
+
 ];
 
 const dataportfolio = [{
-        img: "/image1.png",
-        description: "A modern e-commerce store built with React, featuring a user-friendly interface, secure payments, and efficient product browsing..",
-        link: "https://perfacto.netlify.app/",
-    },
-    {
-        img: "/image2.png",
-        description: "A personal website made with Bootstrap, responsive to all screens.",
-        link: "https://gehadsayed1.github.io/the-first-exam/",
-    },
-    {
-        img: "/image3.png",
-        description: "A personal website made with Bootstrap, responsive to all screens.",
-        link: "https://templettow.netlify.app/",
-    },
-    {
-        img: "/image4.png",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "https://grand-kheer-42a489.netlify.app/",
-    },
-    {
-        img: "/image5.png",
-        description: "Responsive to-do list, you can add an image.",
-        link: "https://gehadsayed1.github.io/task-list/",
-    },
-    {
-        img: "/image6.png",
-        description: "The dash board is organized, has everything that will be needed, and is responsive.",
-        link: "https://gehadsayed1.github.io/Dashbord/",
-    },
+    img: "/image1.png",
+    description: "A modern e-commerce store built with React, featuring a user-friendly interface, secure payments, and efficient product browsing..",
+    link: "https://perfacto.netlify.app/",
+},
 
-    {
-        img: "/image7.png",
-        description: " website made with css, responsive to all screens.",
-        link: "https://gehadsayed1.github.io/template-three/",
-    },
-    {
-        img: "/image8.png",
-        description: " website made with css, responsive to all screens.",
-        link: "https://gehadsayed1.github.io/p5web/",
-    },
-    {
-        img: "/image9.png",
-        description: " website made with css ,html,js, responsive to all screens.",
-        link: "https://gehadsayed1.github.io/temple-tow/",
-    },
-    {
-        img: "/image10.png",
-        description: " website made with css ,html,js, responsive to all screens.",
-        link: "https://gehadsayed1.github.io/jobs/",
-    },
-    {
-        img: "/image11.png",
-        description: " website made with css ,html,js,bootstrap, responsive to all screens..",
-        link: "https://gehadsayed1.github.io/template-bootstrap/",
-    },
-    {
-        img: "/image12.png",
-        description: " website made with css ,html,js,bootstrap, responsive to all screens..",
-        link: "https://gehadsayed1.github.io/template-three/",
-    },
+{
+    img: "/image2.png",
+    description: "A personal website made with Bootstrap, responsive to all screens.",
+    link: "https://gehadsayed1.github.io/the-first-exam/",
+},
+{
+    img: "/image14.png",
+    description: "A system for managing expenses, basic and additional salaries",
+    link: "https://expense-management-8d0ef.web.app/",
+},
+{
+    img: "/image3.png",
+    description: "A personal website made with Bootstrap, responsive to all screens.",
+    link: "https://templettow.netlify.app/",
+},
+
+{
+    img: "/image4.png",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://grand-kheer-42a489.netlify.app/",
+},
+
+{
+    img: "/image5.png",
+    description: "Responsive to-do list, you can add an image.",
+    link: "https://gehadsayed1.github.io/task-list/",
+},
+{
+    img: "/image15.png",
+    description: "Online clothing store with AI recommendation system",
+    link: "https://robyana-team.web.app/",
+},
+{
+    img: "/image6.png",
+    description: "The dash board is organized, has everything that will be needed, and is responsive.",
+    link: "https://gehadsayed1.github.io/Dashbord/",
+},
+
+{
+    img: "/image7.png",
+    description: " website made with css, responsive to all screens.",
+    link: "https://gehadsayed1.github.io/template-three/",
+},
+
+{
+    img: "/image8.png",
+    description: " website made with css, responsive to all screens.",
+    link: "https://gehadsayed1.github.io/p5web/",
+},
+{
+    img: "/image9.png",
+    description: " website made with css ,html,js, responsive to all screens.",
+    link: "https://gehadsayed1.github.io/temple-tow/",
+},
+{
+    img: "/image13.png",
+    description: " Courses Company system combines employee management and student management",
+    link: "https://exam-brooklyn.onrender.com/",
+},
+
+{
+    img: "/image10.png",
+    description: " website made with css ,html,js, responsive to all screens.",
+    link: "https://gehadsayed1.github.io/jobs/",
+},
+{
+    img: "/image16.png",
+    description: " Landing Page to thank you in Dubai for clearing travel papers",
+    link: "https://absherbelkhair.com/",
+},
+
+{
+    img: "/image12.png",
+    description: " website made with css ,html,js,bootstrap, responsive to all screens..",
+    link: "https://gehadsayed1.github.io/template-three/",
+},
+{
+    img: "/image17.png",
+    description: " Landing Page for a Software Company in Dubai",
+    link: "https://my-software-landing.web.app/",
+},
 ];
 
 const contactConfig = {
@@ -152,7 +178,7 @@ const contactConfig = {
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_lexgsjl",
-YOUR_TEMPLATE_ID: "template_gq9f0hn",
+    YOUR_TEMPLATE_ID: "template_gq9f0hn",
     YOUR_USER_ID: "QR_yTc93M0JDJwxC-",
 };
 
